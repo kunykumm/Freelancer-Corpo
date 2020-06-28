539e9b28a58a9032329b13f97f9849bc9ad8a8d2
kunykumm <55748680+kunykumm@users.noreply.github.com>
2020-06-28 16:06:32 +0200

    DAL Testing

---
+++ FreelancerCorp/FreelamcerCorp.DAL.Tests/Initializer.cs
@@ -0,0 +1,21 @@
+﻿using Castle.Windsor;
+using FreelamcerCorp.DAL.Tests.Config;
+using FreelancerCorp.DataAccessLayer;
+using NUnit.Framework;
+using System.Data.Entity;
+
+namespace FreelamcerCorp.DAL.Tests
+{
+    [SetUpFixture]
+    public class Initializer
+    {
+        internal static readonly IWindsorContainer Container = new WindsorContainer();
+
+        [OneTimeSetUp]
+        public void InitializeBusinessLayerTests()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<FreelancerCorpDbContext>());
+            Container.Install(new EntityFrameworkTestInstaller());
+        }
+    }
+}
